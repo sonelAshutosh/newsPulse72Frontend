30070d5c50732ce3a194462a22a432c41694fd6d
Ashutosh Sonel <ashutoshhoonmain@gmail.com>
2024-04-27 03:13:40 +0530

    Changed the baseUrl

--- src/axios.jsx
+++ src/axios.jsx
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 // we need to pass the baseURL as an object
 const API = axios.create({
-  baseURL: 'http://localhost:5500/',
-  // baseURL: 'https://news-pulse72-backend.vercel.app/',
+  // baseURL: 'http://localhost:5500/',
+  baseURL: 'https://news-pulse72-backend.vercel.app/',
 })
 
 export default API
